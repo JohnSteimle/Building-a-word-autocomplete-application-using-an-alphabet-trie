90b7fbe9ca796b3fa6211500d906d01f68baa61e
JohnSteimle <steimle.7@wright.edu>
2023-04-11 18:53:05 -0400

    almost totally done

--- Project5.cpp
+++ Project5.cpp
@@ -9,4 +9,25 @@ int main() {
     Trie tree = Trie();
     ifstream myReadFile;
     myReadFile.open("wordlist_windows.txt");
+    string word;
+    while(myReadFile >> word){
+        tree.insert(word);
+    }
+    while(true){
+    string prefix;
+    cout << "Please enter a word prefix (or press enter to exit):";
+    cin >> prefix;
+    int numComp = tree.completeCount(prefix);
+    cout << "There are " << numComp << " matches for the prefix " << prefix << " Show completions?(yes or no)" << endl;
+    string show;
+    cin >> show;
+    vector<string> words = tree.complete(prefix);
+    if(show == "yes"){
+        cout << "---------";
+        for(int i=0; i<words.size(); i++){
+            cout << words.at(i) << endl;
+        }
+    }
+    }
+    
 }
