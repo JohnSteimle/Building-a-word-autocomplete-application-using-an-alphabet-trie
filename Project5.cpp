612b3391ff6715c0318a68d335072ef7b45cdbca
JohnSteimle <steimle.7@wright.edu>
2023-04-11 18:35:36 -0400

    starting input from file

--- Project5.cpp
+++ Project5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <exception>
@@ -6,22 +7,6 @@
 using namespace std;
 int main() {
     Trie tree = Trie();
-    cout << tree.insert("hello");
-    cout << tree.insert("an");
-    cout << tree.insert("and") << endl;
-    cout << tree.find("an");
-    cout << tree.find("and");
-    cout << tree.find("andy");
-    cout << tree.insert("andy");
-    cout << tree.find("andy");
-    cout << tree.count() << endl;
-    cout << tree.insert("andling");
-    cout << tree.insert("andlingl");
-    cout << tree.insert("andlingkc") << endl;
-    cout << tree.completeCount("an") << endl;
-    cout << tree.getSize() << endl;
-    vector<string> words = tree.complete("an");
-    for(int i=0; i<words.size(); i++){
-        cout << words.at(i) << endl;
-    }
+    ifstream myReadFile;
+    myReadFile.open("wordlist_windows.txt");
 }
